9dce1a566eb34b2be0a24962e65876dfe52317ff
Kylie Clark <kyliedarlene@icloud.com>
2024-02-27 23:21:01 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,73 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = []
+
+        for message in Message.query.order_by(Message.created_at.asc()).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+            
+        response = make_response(
+            messages,
+            200
+        )
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        
+        new_message = Message(
+            body=data.get("body"),
+            username=data.get("username")
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(new_message_dict),
+            201
+        )
+
+    return response
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+
+        message.body = data.get("body")
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response = make_response(
+            {
+                "delete_successful": True,
+                "message": "Review deleted."
+            },
+            200
+        )
+    
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
